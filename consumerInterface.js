98f610b14fbb9c3c3920ad0d3b54b489622e5417
jbrierty <jbrierty@earthlink.net>
2017-05-14 09:01:18 -0700

    added console logs to provider and consumer Interface versions

---
+++ consumerInterface.js
@@ -0,0 +1,47 @@
+let W1 = 100;
+let W2 = 100;
+let b1 = 0;
+let EtoG1 = 0;
+let SEU1 = 0;
+let SEU2 = 0;
+let EFG1 = 0;
+let EFG2 = 0;
+function viEnAcountC(verification, energyGen1, EU1, EU2) {
+	console.log("verification = " + verification + ",energyGen1 =" + energyGen1 + " , EU1 =" + EU1 + " ,EU2 = " + EU2);
+	if (verification){
+		//battery between full and empty
+		if ((b1+energyGen1-(EU1 + EU2) <= 100)&&(b1+energyGen1-(EU1 + EU2) >= 0)) {
+			b1 += energyGen1 - (EU1 + EU2);
+			SEU1 += EU1;
+			SEU2 += EU2;
+			W1 += EU2;
+			W2 -= EU2;
+		//battery has run out
+		}else if(b1+energyGen1-(EU1 + EU2) < 0){
+			b1 += energyGen1;
+			//enough energy for the primary user
+			if(b1 - EU1 >= 0){
+				SEU1 += EU1;
+				b1 -= EU1;
+				SEU2 += b1;
+				EFG2 += EU2 - b1;
+				b1 = 0;
+			//not enough energy for either
+			}else{
+				SEU1 += b1;
+				EFG1 += (EU1-b1);
+				b1 = 0;
+				EFG2 += EU2;
+			}
+		//battery is full
+		}else{
+			EtoG1 += b1 + (energyGen1 - 100) - (EU1 + EU2);
+			SEU1 += EU1;
+			SEU2 += EU2;
+		}
+	}
+console.log();
+console.log("wallet: " + W2)
+console.log("solar energy used: " + SEU2)
+console.log("energy from grid used: " + EFG2)
+}
